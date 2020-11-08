ba0bd6e29d96d7965238b4d01bef64144ac49823
davidalvarez19 <52938488+davidalvarez19@users.noreply.github.com>
2020-11-08 16:08:00 -0500

    Add files via upload

---
+++ JSfinalProject/js/gregBoggle.js
@@ -0,0 +1,32 @@
+function words(x)
+{
+	switch (x)
+	{
+	case 1:
+		var word = new Array("balte","table","hat","tab","belt","lab","eat","tea","ate","tale","bale","let","bet","teal","late","beat");
+		break;		
+	case 2:
+		var word = new Array("atwre","water","wet","wear","tear","war","ret","rate","eat","ate","tea","awe","raw","rat","wart","art","tar");
+		break;
+	case 3:
+		var word = new Array("dclaen","can","cane","and","clan","lane","lean","lend","land","den","dean","dance","lance","clean","deal","ale","dale","candle","clad");
+		break;
+	case 4:
+		var word = new Array("aepinlar","air","airplane","plane","plan","lane","lean","pane","ear","near","nap","nape","lair","pen","pan","ape","leap","ale","peal","nap","rap","par", "pare", "pale", "are", "rail", "ail", "pail", "nail", "air", "pair", "ran", "pin", "pine", "line", "nip", "rip", "ripe", "lip", "earn", "learn", "ire");
+		break;
+	case 5:
+		var word = new Array("redykboa","keyboard","key","board","bored","bore","bark","dark","dork","oar","boar","ark","dare","bare","are","red","rod","road","bode","rode","ode","bread", "read", "bead", "bred", "break", "drey", "day", "boy", "broke", "rake", "bake", "ear", "dear", "bear", "dye", "dyer", "doer", "oak", "boa", "doe", "okay","dab", "bade", "ade", "drake", "bard", "yard", "year", "beak", "beard", "bad", "bed", "bay");
+		break;
+    case 6:
+        var word = new
+        Array("dgusie","gudes","guide","guids","guise","dies","digs","dues","dugs","egis","geds","gids","gied","gies","gude","guid","ides","side","sued","used","dei","des","die","dig","dis","due","dug","dui","eds","ged","gid","gie","ids","seg","sei","sue","use","de","di","ed","es","gi","id","is","si","us");
+        break;
+    case 7:
+        var word = new Array("fbesak","bakes","beaks","fakes","bake","base","bask","beak","fabs","fake","kabs","kaes","kafs","keas","kefs","sabe","safe","sake","abs","ask","bas","bes","efs","fab","fas","fes","kab","kae","kaf","kas","kea","kef","sab","sae","sea","ska","ab","ae","as","ba","be","ef","es","fa","fe","ka");
+        break;
+    case 8:
+        var word = new Array("ntuirp","turnip","input","print","purin","rutin","unrip","pint","pirn","ptui","punt","puri","ruin","runt","trip","turn","unit","nit","nut","pin","pit","piu","pun","pur","put","rin","rip","run","rut","tin","tip","tui","tun","tup","urn","urp","in","it","nu","pi","ti","un","up","ut");
+        break;
+	}
+	return word;
+}
